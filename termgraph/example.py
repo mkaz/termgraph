5a8dbed0721a894177f2e4293786b08a8ce3cb1b
Marcus Kazmierczak <marcus@mkaz.com>
2021-06-20 07:16:11 -0700

    Fix example import name

--- termgraph/example.py
+++ termgraph/example.py
@@ -1,4 +1,4 @@
-from oop import Data, BarChart, Args, Colors
+from module import Data, BarChart, Args, Colors
 
 data = Data([[765, 787], [781, 769]], ["6th G", "7th G"], ["Boys", "Girls"])
 chart = BarChart(
